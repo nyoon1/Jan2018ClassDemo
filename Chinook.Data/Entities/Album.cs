a9d6c7c982df47a45e7e9a09aa8aa81ace42eeb2
nyoon1 <35244631+nyoon1@users.noreply.github.com>
2018-01-17 18:45:09 -0700

    20180117

---
+++ Chinook.Data/Entities/Album.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        [StringLength(120, ErrorMessage = "Album title has a mximum of 120 characters")]
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        [StringLength(50, ErrorMessage ="Album labem has a maximum of 50 characters")]
+        public string ReleaseLabel { get; set; }
+
+        //navigational properties
+        //this class is on the middle of Artist and Track that is why we have two properties
+        public virtual Artist Artist { get; set; }
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
---
+++ Chinook.Data/Entities/Track.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Tracks")]
+    public class Track
+    {
+        [Key]
+        public int TrackId { get; set; }
+        [Required ()]
+        [StringLength(200, ErrorMessage = "Album Title has a maximum of 200 characters")]
+        public string Name { get; set; }
+        public int? AlbumId { get; set; }
+        public int MediaTypeId { get; set; }
+        public int? GeneralId { get; set; }
+        [StringLength(220, ErrorMessage = "Composer has a maximum of 220 characters")]
+        public string Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //Navigational properties
+        public virtual Album Album { get; set; }
+
+
+    }
+}
