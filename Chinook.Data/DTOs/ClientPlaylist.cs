c8d13661684da92452e5e398a5a1e5cb996b1489
nyoon1 <35244631+nyoon1@users.noreply.github.com>
2018-02-12 18:28:39 -0700

    20180212 - Repeater
    1st commit

---
+++ Chinook.Data/DTOs/ClientPlaylist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using Chinook.Data.POCOs;
+#endregion
+namespace Chinook.Data.DTOs
+{
+    public class ClientPlaylist
+    {
+        public string playlist { get; set; }
+        public List<TracksAndGenre> songs { get; set; }
+    }
+}
