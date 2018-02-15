8eb6fce61a9c09364d07fa042e76eeb73f3cde28
nyoon1 <35244631+nyoon1@users.noreply.github.com>
2018-02-14 18:47:41 -0700

    20180214 - Repeater

---
+++ Chinook.Data/DTOs/EmployeeClients.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chinook.Data.POCOs;
+
+namespace Chinook.Data.DTOs
+{
+    public class EmployeeClients
+    {
+        public string Name { get; set; }
+        public int ClientCount { get; set; }
+        public IEnumerable<ClientInfo> ClientList { get; set; }
+    }
+}
---
+++ ChinookSystem/BLL/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.DTOs;
+using Chinook.Data.POCOs;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class EmployeeController
+    {
+        //our access  to the DB is via the Context class
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<EmployeeClients> Employee_GetClientList()
+        {
+            using (var context = new ChinookContext())
+            {
+
+                var results = from employeeRow in context.Employees
+                              where employeeRow.Title.Contains("Support")
+                              orderby employeeRow.LastName, employeeRow.FirstName
+                              select new
+                              {
+                                  Name = employeeRow.LastName + ", " + employeeRow.FirstName,
+                                  //title = employeeRow.Title,
+                                  ClientCount = employeeRow.Customers.Count(),
+                                  ClientList = from customerRowOfemployeeRow in employeeRow.Customers
+                                               orderby customerRowOfemployeeRow.LastName,
+                                                        customerRowOfemployeeRow.FirstName
+                                               select new ClientInfo
+                                               {
+                                                   Client = customerRowOfemployeeRow.LastName + ", " + customerRowOfemployeeRow.FirstName,
+                                                   Phone = customerRowOfemployeeRow.Phone
+                                               }
+
+                              };
+                return results.ToList();
+            }
+
+        }
+    }
+}
